404922e0af297d8e29734847efcc002e98de5585
Veltris Admin <veltris.compliance@gmail.com>
2026-04-10 01:24:30 +0100

    Form Update

---
+++ scripts/form.js
@@ -0,0 +1,44 @@
+// Product Array provided by the assignment
+const products = [
+  {
+    id: "fc-1888",
+    name: "flux capacitor",
+    averagerating: 4.5
+  },
+  {
+    id: "fc-2050",
+    name: "power laces",
+    averagerating: 4.7
+  },
+  {
+    id: "fs-1987",
+    name: "time circuits",
+    averagerating: 3.5
+  },
+  {
+    id: "ac-2000",
+    name: "low voltage reactor",
+    averagerating: 3.9
+  },
+  {
+    id: "jj-1969",
+    name: "warp equalizer",
+    averagerating: 5.0
+  }
+];
+
+// Populate the product select options
+const productSelect = document.getElementById("product-select");
+
+if (productSelect) {
+    products.forEach(product => {
+        let option = document.createElement("option");
+        option.value = product.id; // Assign the ID to the value attribute
+        option.textContent = product.name; // Display the product name
+        productSelect.appendChild(option);
+    });
+}
+
+// Set footer dates
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+document.getElementById("lastModified").textContent = `Last Modification: ${document.lastModified}`;
